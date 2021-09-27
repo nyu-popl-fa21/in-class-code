c08f525c08a235415d3b2c73669422730d988b48
Thomas Wies <wies@cs.nyu.edu>
2021-09-27 13:53:35 -0400

    code from class05

--- src/main/scala/popl/class05/numbers.scala
+++ src/main/scala/popl/class05/numbers.scala
@@ -1,5 +1,17 @@
 package popl.class05
 
 object numbers {
+  sealed abstract class N
+  case object Zero extends N
+  case class Succ(x: N) extends N
 
+  def D(x: N): Int = x match {
+    case Zero => 0
+    case Succ(xp) => 1 + D(xp)
+  }
+
+  def plus(x: N, y: N): N = x match {
+    case Zero => y
+    case Succ(xp) => plus(xp, Succ(y))
+  }
 }
